88e13df8f70369b8f1ba7054d428ea72244bd814
Romain Cambonie <romain.cambonie@gmail.com>
2023-06-05 20:05:12 +0200

    refactor: remove SubmitComponent

--- src/features/authentication/components/index.ts
+++ src/features/authentication/components/index.ts
@@ -1,11 +1,9 @@
 import { CodeFieldComponent, PasswordFieldComponent, TermsFieldComponent, UsernameFieldComponent } from './fields';
-import { SubmitComponent } from './submit/submit.component';
 
 // eslint-disable-next-line @typescript-eslint/typedef
 export const COMPONENTS = [
   CodeFieldComponent,
   PasswordFieldComponent,
   TermsFieldComponent,
-  UsernameFieldComponent,
-  SubmitComponent
+  UsernameFieldComponent
 ];
