c024fac38d342869787099ca8fad3f98d1f8c754
Romain Cambonie <romain.cambonie@gmail.com>
2023-06-17 16:19:05 +0200

    feat: adding badges emoji and styles to improve visuals

--- src/features/planning/components/fares/fare-details/fare-details.component.ts
+++ src/features/planning/components/fares/fare-details/fare-details.component.ts
@@ -4,8 +4,45 @@ import { FareForDate } from '../../../providers';
 @Component({
   selector: 'app-fare-details',
   templateUrl: './fare-details.component.html',
-  styleUrls: ['./fare-details.component.css']
+  styleUrls: ['./fare-details.component.scss']
 })
 export class FareDetailsComponent {
   @Input({ required: true }) public fare!: FareForDate;
+
+  public getKindBadgeClass(kind: string): string {
+    switch (kind) {
+      case 'go-back':
+        return 'badge rounded-pill text-bg-primary';
+      case 'one-way':
+        return 'badge rounded-pill text-bg-secondary';
+      case 'outward':
+        return 'badge rounded-pill text-bg-success';
+      default:
+        return 'badge';
+    }
+  }
+
+  public getNatureBadgeClass(nature: string): string {
+    switch (nature) {
+      case 'medical':
+        return 'badge rounded-pill text-bg-success';
+      case 'standard':
+        return 'badge rounded-pill text-bg-secondary';
+      default:
+        return 'badge';
+    }
+  }
+
+  public getStatusBadgeClass(fareStatus: string): string {
+    switch (fareStatus) {
+      case 'scheduled':
+        return 'badge rounded-pill text-bg-info';
+      case 'in-progress':
+        return 'badge rounded-pill text-bg-warning';
+      case 'finished':
+        return 'badge rounded-pill text-bg-primary';
+      default:
+        return 'badge';
+    }
+  }
 }
