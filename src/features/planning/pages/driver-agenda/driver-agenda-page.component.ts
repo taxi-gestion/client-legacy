a59cf05a92c67d9381991a0ecb852e71ad8a5b0d
Romain Cambonie <romain.cambonie@gmail.com>
2023-06-28 19:25:38 +0200

    feat: agenda view use user attribute (#22)

--- src/features/planning/pages/driver-agenda/driver-agenda-page.component.ts
+++ src/features/planning/pages/driver-agenda/driver-agenda-page.component.ts
@@ -4,6 +4,7 @@ import { FARES_FOR_DATE_QUERY, FaresForDateQuery } from '@features/planning';
 import { ActivatedRoute } from '@angular/router';
 import { filterByPlanning, formatDateDDMMYYYY, toFaresForDatePresentation } from '../../common/fares.presenter';
 import { DailyAgenda } from '../../common/fares.presentation';
+import { SESSION_PERSISTENCE, TokenSession } from '../../../authentication';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -14,17 +15,17 @@ export class DriverAgendaPage {
   private readonly _dateFromUrl: string | null = this._route.snapshot.params['date'];
 
   // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-  private readonly _planningFromUrl: string = this._route.snapshot.params['planning'] ?? '';
   private readonly _selectedDate: Date = this._dateFromUrl == null ? new Date() : new Date(this._dateFromUrl);
   public today: string = formatDateDDMMYYYY(this._selectedDate);
 
   public constructor(
     private readonly _route: ActivatedRoute,
-    @Inject(FARES_FOR_DATE_QUERY) private readonly _faresForDateQuery: FaresForDateQuery
+    @Inject(FARES_FOR_DATE_QUERY) private readonly _faresForDateQuery: FaresForDateQuery,
+    @Inject(SESSION_PERSISTENCE) private readonly _tokenSession: TokenSession
   ) {}
 
   public readonly agenda$: Observable<DailyAgenda> = this._faresForDateQuery(this._selectedDate).pipe(
     map(toFaresForDatePresentation),
-    map(filterByPlanning(this._planningFromUrl))
+    map(filterByPlanning(this._tokenSession.idTokenPayload('custom:planning_identifier') as string))
   );
 }
