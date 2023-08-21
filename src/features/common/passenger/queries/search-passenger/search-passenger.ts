42de54b4d229e23be9f834480b4228677bf5a396
Romain Cambonie <romain.cambonie@gmail.com>
2023-08-21 13:16:01 +0200

    feat: use route instead of mock

--- src/features/common/passenger/queries/search-passenger/search-passenger.ts
+++ src/features/common/passenger/queries/search-passenger/search-passenger.ts
@@ -1,36 +1,9 @@
 import { HttpClient } from '@angular/common/http';
-import { Observable, of } from 'rxjs';
+import { Observable } from 'rxjs';
 import { SearchPassengerQuery } from '../../providers';
 import { Passenger } from '../../definitions';
 
 export const searchPassengersQuery$ =
-  (_httpClient: HttpClient): SearchPassengerQuery =>
+  (httpClient: HttpClient): SearchPassengerQuery =>
   (_search: string): Observable<Passenger[]> =>
-    of([
-      {
-        firstname: 'Romain',
-        lastname: 'Cambonie',
-        phone: '0684319515'
-      },
-      {
-        firstname: 'Marc',
-        lastname: 'Gavanier',
-        phone: '0684319516'
-      },
-      {
-        firstname: 'Marie',
-        lastname: 'Duchamps',
-        phone: '0684319517'
-      },
-      {
-        firstname: 'Pierre-Antoine',
-        lastname: 'De Lacolline',
-        phone: '0684319518'
-      }
-    ]);
-//httpClient
-//  .get<CognitoPassengersTransfer[]>(
-//    `/api/fares-for-date/${search}`
-//  )
-//  .pipe(map(toPassengers));
-//
+    httpClient.get<Passenger[]>(`/api/list-passengers`);
