b20746cf833cd8232ec94d57579fc9ffbffc3d5b
Romain Cambonie <romain.cambonie@gmail.com>
2023-09-13 18:38:18 +0200

    feat: directive that add or remove bootstrap validation classes (#112)

--- src/features/planning/directives/index.ts
+++ src/features/planning/directives/index.ts
@@ -1,6 +1,8 @@
 import { HorizontalMouseScrollDirective } from './horizontal-mouse-scroll.directive';
+import { FormControlErrorDirective } from './from-control-error.directive';
 
 export * from './horizontal-mouse-scroll.directive';
+export * from './from-control-error.directive';
 
 // eslint-disable-next-line @typescript-eslint/typedef
-export const DIRECTIVES = [HorizontalMouseScrollDirective];
+export const DIRECTIVES = [HorizontalMouseScrollDirective, FormControlErrorDirective];
