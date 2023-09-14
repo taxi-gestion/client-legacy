ce63c4db05adda62f9ba40d903c7158090c4d9c2
Romain Cambonie <romain.cambonie@gmail.com>
2023-09-14 16:13:39 +0200

    feat: added inputs to component to handle form arrays (#115)

--- src/features/planning/components/errors/form-control-errors/form-control-errors.presenter.ts
+++ src/features/planning/components/errors/form-control-errors/form-control-errors.presenter.ts
@@ -1,6 +1,9 @@
-export type FormControlErrorsNames = 'lastname';
+export type FormControlErrorsNames = 'destinationName' | 'lastname' | 'phoneNumber' | 'phoneType';
 
 export const FORM_CONTROL_ERROR_MESSAGES: Record<FormControlErrorsNames, (controlValue: unknown) => string> = {
-  lastname: (controlValue: unknown): string =>
-    `Une valeur est obligatoire pour le nom de famille: "${String(controlValue)}" est incorrect`
+  phoneType: (): string => `Une valeur est obligatoire pour le type de téléphone.`,
+  phoneNumber: (controlValue: unknown): string =>
+    `Le numéro de téléphone: "${String(controlValue)}" ne respecte pas le format français.`,
+  lastname: (): string => `Une valeur est obligatoire pour le nom de famille.`,
+  destinationName: (): string => `Une valeur est obligatoire pour la destination.`
 };
