765ce0d27d47ea08ccb2691377e75812a5814307
Romain Cambonie <romain.cambonie@gmail.com>
2023-10-03 16:56:06 +0200

    feat: redirect billing user to billing page on login (#138)

--- src/features/planning/guards/can-activate-planning-redirect.guard.ts
+++ src/features/planning/guards/can-activate-planning-redirect.guard.ts
@@ -17,6 +17,8 @@ export class CanActivatePlanningRedirectGuard {
     if (isManager(userGroups)) return navigateToDaily(this._router)(route.params['date']);
     // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
     if (isDriver(userGroups)) return navigateToAgenda(this._router)(route.params['date']);
+    // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+    if (isBilling(userGroups)) return navigateToBilling(this._router)(route.params['date']);
 
     return of(false);
   };
@@ -25,6 +27,8 @@ export class CanActivatePlanningRedirectGuard {
 const hasNoGroups = (userGroups: string[]): userGroups is [] => userGroups.length === 0;
 const isManager = (userGroups: string[]): boolean => userGroups.includes('manager');
 const isDriver = (userGroups: string[]): boolean => userGroups.includes('driver');
+const isBilling = (userGroups: string[]): boolean => userGroups.includes('billing');
+
 const isBothManagerAndDriver = (userGroups: string[]): boolean => isManager(userGroups) && isDriver(userGroups);
 const navigateToMissingAdminConfiguration = (router: Router): Observable<boolean> =>
   from(router.navigate(['/planning/missing-admin-configuration']));
@@ -38,3 +42,8 @@ const navigateToAgenda =
   (router: Router) =>
   (date: string | undefined): Observable<boolean> =>
     date == null ? from(router.navigate(['/planning/agenda/'])) : from(router.navigate(['/planning/agenda/', date]));
+
+const navigateToBilling =
+  (router: Router) =>
+  (date: string | undefined): Observable<boolean> =>
+    date == null ? from(router.navigate(['/planning/billing/'])) : from(router.navigate(['/planning/billing/', date]));
