0dffec2912654c97b7425a6a2f49195e0e49d675
Romain Cambonie <romain.cambonie@gmail.com>
2023-10-06 10:51:42 +0200

    build: fixed configuration for test in module paths (#142)

--- jest.config.cjs
+++ jest.config.cjs
@@ -1,5 +1,10 @@
-// Keep this file as .cjs to be able to run without ts-node.
-/** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
-  preset: 'jest-preset-angular'
+  preset: 'jest-preset-angular',
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  moduleNameMapper: {
+    '@codecs': '<rootDir>/src/codecs',
+    '@definitions': '<rootDir>/src/definitions',
+    '@features/(.*)': '<rootDir>/src/features/$1'
+  }
 };
--- src/features/common/fare/fare.presenter.spec.ts
+++ src/features/common/fare/fare.presenter.spec.ts
@@ -1,14 +1,11 @@
-// Fail because of angular / jest config
-/*
 import { isMedicalDrive } from './fare.presenter';
 import { Nature } from '@definitions';
 
 describe('fare presenter', (): void => {
   it.each([
-    ['standard' as const, true],
-    ['medical' as const, false]
+    ['standard' as const, false],
+    ['medical' as const, true]
   ])('isMedicalDrive should return %s for %s', (nature: Nature['nature'], expectedResult: boolean): void => {
     expect(isMedicalDrive(nature)).toBe(expectedResult);
   });
 });
-*/
