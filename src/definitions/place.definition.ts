b79a71a2f9133403313d8c72fb0d998a7933272f
Romain Cambonie <romain.cambonie@gmail.com>
2024-01-22 18:47:43 +0100

    fix: removing location (#167)

--- src/definitions/place.definition.ts
+++ src/definitions/place.definition.ts
@@ -1,8 +1,4 @@
-/* eslint-disable @typescript-eslint/no-shadow */
-import { Location } from './location.definition';
-
 export type Place = {
   context: string;
   label: string;
-  location: Location;
 };
--- src/features/common/place/definitions/place.definition.ts
+++ src/features/common/place/definitions/place.definition.ts
@@ -1,10 +1,4 @@
-export type LocationValues = {
-  latitude: number;
-  longitude: number;
-};
-
 export type PlaceValues = {
   context: string;
   label: string;
-  location: LocationValues;
 };
