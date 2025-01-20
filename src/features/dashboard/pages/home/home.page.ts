70ba99712e5aed4001ffab9878edf312e8062c5b
Romain Cambonie <romain.cambonie@gmail.com>
2025-01-20 10:45:03 +0100

    feat: managers only do not have agenda

--- src/features/dashboard/pages/home/home.page.ts
+++ src/features/dashboard/pages/home/home.page.ts
@@ -1,7 +1,21 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Inject } from '@angular/core';
+import { Session, SESSION_PERSISTENCE } from '../../../authentication';
 
 @Component({
   changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './home.page.html'
 })
-export class HomePage {}
+export class HomePage {
+  public userGroups: string[];
+  public constructor(@Inject(SESSION_PERSISTENCE) private readonly _session: Session) {
+    this.userGroups = this._session.groups();
+  }
+
+  public isManagerAndDriver(): boolean {
+    return (this.userGroups.includes('manager') && this.userGroups.includes('driver')) || this.userGroups.includes('developer');
+  }
+
+  public isManagerOnly(): boolean {
+    return this.userGroups.includes('manager') && !this.userGroups.includes('driver');
+  }
+}
